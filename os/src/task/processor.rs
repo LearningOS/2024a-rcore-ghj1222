88ba280a44c88b9d6e1ee9b5a80ac8d59ae2f837
ghj1222 <1342330485@qq.com>
2024-10-24 20:49:17 +0800

    merge ch5-3

--- os/src/task/processor.rs
+++ os/src/task/processor.rs
@@ -58,6 +58,7 @@ pub fn run_tasks() {
         if let Some(task) = fetch_task() {
             let idle_task_cx_ptr = processor.get_idle_task_cx_ptr();
             // access coming task TCB exclusively
+            task.add_stride();
             let mut task_inner = task.inner_exclusive_access();
             let next_task_cx_ptr = &task_inner.task_cx as *const TaskContext;
             task_inner.task_status = TaskStatus::Running;
